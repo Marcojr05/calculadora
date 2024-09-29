e0a98c0dbac935f2a46c1651cf32c15a20cfd128
Marcojr05 <marcoantoniojr052005@gmail.com>
2024-09-29 19:04:48 -0300

    subindo

--- js/script.js
+++ js/script.js
@@ -20,6 +20,7 @@ function verificarCalc() {
     document.getElementById("resp").innerText = resultado
 }
 
+
 function verificarSal() {
     let salar = Number(document.getElementById("salar").value)
     let senio = document.getElementById("calcSal").value
@@ -36,4 +37,43 @@ function verificarSal() {
             break
     }
     document.getElementById("sal").innerText = `Seu novo salário é R$${aumento},00`
+}
+
+
+ function calcularTotal() {
+
+    const pedido = document.getElementById("pedido").value.split(',');
+
+    let total = 0;
+
+    pedido.forEach(codigo => {
+        codigo = codigo.trim();
+        switch (codigo) {
+            case "001":
+                total += 30.00;
+                break;
+            case "002":
+                total += 45.00;
+                break;
+            case "003":
+                total += 60.00;
+                break;
+            case "004":
+                total += 7.00;
+                break;
+            case "005":
+                total += 10.00;
+                break;
+            case "006":
+                total += 14.00;
+                break;
+            case "007":
+                total += 18.00;
+                break;
+            default:
+                alert(`O item de código ${codigo} não existe no cardápio.`);
+        }
+    });
+
+    document.getElementById("total").innerText = "R$ " + total.toFixed(2);
 }
